96c98b74e48e60362b7b121acfabddb4528332ee
nauman0786khan <109727158+nauman0786khan@users.noreply.github.com>
2023-12-21 10:09:33 +0000

    apicatdone

--- Code/Script.js
+++ Code/Script.js
@@ -6,6 +6,21 @@ return value.json();
 }).then((data)=>{
 
 console.log(data);
+let dd=""
+data.map((ff)=>{
 
+     dd+=`<div id="${ff.id}">
+    <img src="${ff.image}" height="250px" width="100%" alt="">
+    <p>${ff.name}</p2>
+    </div>`;
+
+});
+
+
+
+
+
+
+document.getElementById("catroot").innerHTML = dd;
 
 });

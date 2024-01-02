4987c370c54b52f497a7656c8e0b49dd75cb264c
nauman0786khan <109727158+nauman0786khan@users.noreply.github.com>
2024-01-02 06:52:30 +0000

    Slider done

---
+++ Code/slider.js
@@ -0,0 +1,35 @@
+$('.owl-carousel').owlCarousel({
+    autoplay:true,
+autoplayTimeout:2000,
+autoplayHoverPause:false,
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:1
+        }
+    }
+})
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})

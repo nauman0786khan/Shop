632a5df8e4aae4c436b12d5495a8ffe4a508858a
nauman0786khan <109727158+nauman0786khan@users.noreply.github.com>
2024-01-02 08:31:03 +0000

    cat slide

--- Code/slider.js
+++ Code/slider.js
@@ -16,20 +16,40 @@ autoplayHoverPause:false,
             items:1
         }
     }
-})
+});
 
-var owl = $('.owl-carousel');
-owl.owlCarousel({
-    items:4,
+// var owl = $('.owl-carousel');
+// owl.owlCarousel({
+//     items:4,
+//     loop:true,
+//     margin:10,
+//     autoplay:true,
+//     autoplayTimeout:1000,
+//     autoplayHoverPause:true
+// });
+// $('.play').on('click',function(){
+//     owl.trigger('play.owl.autoplay',[1000])
+// })
+// $('.stop').on('click',function(){
+//     owl.trigger('stop.owl.autoplay')
+// });
+
+$('.cat').owlCarousel({
+    autoplay:true,
+autoplayTimeout:2000,
+autoplayHoverPause:false,
     loop:true,
     margin:10,
-    autoplay:true,
-    autoplayTimeout:1000,
-    autoplayHoverPause:true
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
 });
-$('.play').on('click',function(){
-    owl.trigger('play.owl.autoplay',[1000])
-})
-$('.stop').on('click',function(){
-    owl.trigger('stop.owl.autoplay')
-})

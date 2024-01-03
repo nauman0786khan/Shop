e5c8cfda66258a1677aece0896b96b92e21f5b62
nauman0786khan <109727158+nauman0786khan@users.noreply.github.com>
2024-01-03 09:10:55 +0000

    done

--- Code/product.js
+++ Code/product.js
@@ -9,12 +9,31 @@ return vv.json();
 // console.log(rr);
 
 let clothes = rr.filter(rr =>rr.category.name =="Clothes")
-console.log(clothes);
-let bb=""
+let Electronics = rr.filter(rr =>rr.category.name =="Electronics")
+console.log(Electronics);
 
-clothes.map((pp)=>{
+
+
+let clo=""
+clothes.map((cl)=>{
+    // console.log(pp);
+    clo+=`<div class="item"><img src="${cl.images}" height="250px" width="250px"><p >${cl.title}</p></div>
+    
+            
+         </div>
+        
+        `;
+    
+    });
+    
+    
+
+
+// Electronics data fetch
+let Elc=""
+Electronics.map((EE)=>{
 // console.log(pp);
-     bb+=`<div class="item"><img src="${pp.images}" height="250px" width="250px"><p >${pp.title}</p></div>
+     Elc+=`<div class="item"><img src="${EE.images}" height="250px" width="250px"><p >${EE.title}</p></div>
 
         
      </div>
@@ -23,7 +42,7 @@ clothes.map((pp)=>{
 
 });
 
-document.getElementById("productroot").innerHTML = bb;
-
+document.getElementById("Electronics").innerHTML = Elc;
+document.getElementById("Clothes").innerHTML = clo;
 
 });

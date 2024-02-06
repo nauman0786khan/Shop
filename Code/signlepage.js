8cd1671bce7008a442af044ba3d89ceda295da3f
nauman0786khan <109727158+nauman0786khan@users.noreply.github.com>
2024-02-06 11:50:42 +0000

    mobiledone

--- Code/signlepage.js
+++ Code/signlepage.js
@@ -17,8 +17,8 @@ Singleproduct.map((va)=>{
     
 console.log(va);
 SPP=` <div class="raw padding flex"><div class="wth"><img src="${va.images}" width="100%" alt=""></div>
-<div class="wth"><h4 class="textgap">${va.category.name}</h4><h2 class="textgap">${va.title}</h2><h1 class="textgap">
-₹${va.price}</h1><ul class="listoffer textgap">
+<div class="wth"><h4 class="textgap">${va.category.name}</h4><h2 class="textgap">${va.title}</h2><h4 class="textgap">
+₹${va.price}</h4><ul class="listoffer textgap">
 <li>Bank Offer10% instant discount on ICICI Bank Credit Cards, up to ₹300, on orders of ₹2,500 and above</li>
 <li>Bank Offer10% off on ICICI Bank Credit Card and EMI Transactions, up to ₹1500, on orders of ₹5,000 and above</li>
 <li>Bank Offer10% off on Bank of Baroda Credit Card and EMI Transactions, up to ₹1500 on orders of ₹5000 and above</li>
@@ -31,4 +31,3 @@ document.getElementById("SPPF").innerHTML = SPP;
 
 
 
-   

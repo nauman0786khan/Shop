0868ec12f82a52f7956ebcba818eb8fb253ed491
nauman0786khan <109727158+nauman0786khan@users.noreply.github.com>
2024-02-06 11:57:32 +0000

    cartcount

--- Code/addcart.js
+++ Code/addcart.js
@@ -39,7 +39,7 @@ cartarr.forEach((item)=>{
    })
    localStorage.setItem("cartn" , cartarr);
 
-   
+   document.getElementById("numberpro").innerHTML= cartarr.length;
    
   
 
@@ -69,7 +69,7 @@ let cartn = localStorage.getItem("cartn");
    console.log(crtn)
   let ctn1 = JSON.parse(crtn);
 console.log(ctn1)
-    document.getElementById("numberpro").innerHTML= ctn1.length;
+    
 
    
  

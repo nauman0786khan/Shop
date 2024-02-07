9a39c6ea6051ad45cc14173a11093432f7bbac15
nauman0786khan <109727158+nauman0786khan@users.noreply.github.com>
2024-02-07 05:34:09 +0000

    doneapi

--- Code/addcart.js
+++ Code/addcart.js
@@ -67,9 +67,9 @@ let cartn = localStorage.getItem("cartn");
 
    let crtn = localStorage.getItem("cartn");
    console.log(crtn)
-  let ctn1 = JSON.parse(crtn);
+  let ctn1 = crtn.split(",")
 console.log(ctn1)
-    
+document.getElementById("numberpro").innerHTML= ctn1.length;  
 
    
  
